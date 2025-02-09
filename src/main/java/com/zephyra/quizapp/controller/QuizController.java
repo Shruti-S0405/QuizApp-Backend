8cbb0f1e9150af85cd2ca4404acf920c91a0de78
shruti-s0405 <shruti.sundar0405@gmail.com>
2025-02-10 01:32:24 +0530

    feat: Fetching Quiz Questions Based on Quiz ID

--- src/main/java/com/zephyra/quizapp/controller/QuizController.java
+++ src/main/java/com/zephyra/quizapp/controller/QuizController.java
@@ -1,14 +1,19 @@
 package com.zephyra.quizapp.controller;
 
-import org.apache.catalina.connector.Response;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.zephyra.quizapp.Model.QuestionWrapper;
+import com.zephyra.quizapp.Model.Response;
 import com.zephyra.quizapp.service.QuizService;
 
 @RestController
@@ -21,5 +26,15 @@ public class QuizController {
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title ){
         return quizService.createQuiz(category, numQ, title);
+    }
+
+    @GetMapping("get/{Id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer Id){
+        return quizService.getQuizQuestions(Id);
+    }
+
+    @PostMapping("submit/{Id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer Id, @RequestBody List<Response> response){
+        return quizService.submitQuiz(Id, response);
     }
 }
--- src/main/java/com/zephyra/quizapp/service/QuizService.java
+++ src/main/java/com/zephyra/quizapp/service/QuizService.java
@@ -8,6 +8,7 @@ import java.util.*;
 import com.zephyra.quizapp.Model.Quiz;
 import com.zephyra.quizapp.Repository.QuestionRepository;
 import com.zephyra.quizapp.Model.Question;
+import com.zephyra.quizapp.Model.QuestionWrapper;
 import com.zephyra.quizapp.Repository.QuizRepository;
 
 @Service
@@ -17,11 +18,27 @@ public class QuizService {
     @Autowired
     QuizRepository quizRepository;
     public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
+        
         List<Question> questions = questionRepository.findRandomQuestionsByCategory(category, numQ);
+        
         Quiz quiz = new Quiz();
         quiz.setTitle(title);
         quiz.setQuestions(questions);
         quizRepository.save(quiz);
+
         return new ResponseEntity<>("Successs", HttpStatus.CREATED);
+    }
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer Id) {
+        Optional<Quiz> quiz = quizRepository.findById(Id);
+        List<Question> questionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+
+        for(Question q : questionsFromDB){
+            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionText(), q.getOption1(), q.getOption2(), q.getOption3());
+            questionsForUser.add(qw);
+        }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
+
     }
 }
